76228babcebb74d8df9dcc3b998875fa2ff71e99
sonerik <thesonerik@gmail.com>
2015-03-28 00:27:21 +0200

    Added some components.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: __dirname,
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
